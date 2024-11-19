19ecb6cf67e0812ca2c2757854b2b4c1ae1844d5
ilyas <oulkaddailyas@gmail.com>
2024-11-19 22:01:14 +0100

    added decoder

--- src/detr/model.py
+++ src/detr/model.py
@@ -63,12 +63,49 @@ class TransformerEncoder(nn.Module):
         return x
 
 class TransformerDecoder(nn.Module):
-    pass
+    def __init__(self, hidden_dim, fc_dim, num_heads, num_obj, activation="relu") -> None:
+        super().__init__()
+        self.object_emb = nn.Embedding(num_embeddings=num_obj, embedding_dim=hidden_dim)
+        self.ln_1 = nn.LayerNorm(hidden_dim)
+        self.w_kv = nn.Linear(hidden_dim, 2*hidden_dim)
+        self.w_q = nn.Linear(hidden_dim, hidden_dim)
+        self.ln_2 = nn.LayerNorm(hidden_dim)
+        self.mlp_1 = nn.Linear(hidden_dim, fc_dim)
+        self.mlp_2 = nn.Linear(fc_dim, hidden_dim)
+        self.activation = getattr(nn.functional, activation)
+        self.num_heads = num_heads
+        self.num_obj = num_obj
 
+    def forward(self, x):
+        tgt = self.object_emb(torch.arange(self.num_obj, device=x.device))
+        tgt = self.ln_1(tgt)
+        x_kv = self.w_kv(x)
+        K, V = x_kv.chunk(2, -1)
+        Q = self.w_q(tgt).unsqueeze(0)
+        print(f"Q shape = {Q.shape}, V shape = {V.shape}, K shape = {K.shape}")
+        Q, K, V = split_into_heads(Q, K, V, num_heads=self.num_heads)
+        attn_out, _ = head_level_self_attention(Q, K, V)
+        attn_out = concat_heads(attn_out)
+        print(f"Attention shape = {attn_out.shape}")
+        attn_out += tgt
+
+        ln_2 = self.ln_2(attn_out)
+        mlp_1 = self.mlp_1(ln_2)
+        x = self.activation(mlp_1)
+        x = self.mlp_2(x)
+        x += attn_out
+
+        return x
+
+
+
+class PredictionHeads(nn.Module):
+    def __init__(self, num_cls, bbox_features) -> None:
+        super().__init__()
 
 
 class DeTr(nn.Module):
-    def __init__(self, backbone, input_shape, fc_dim, num_heads, activ_fn, num_encoder) -> None:
+    def __init__(self, backbone, input_shape, fc_dim, num_heads, activ_fn, num_encoder, num_decoder, num_obj) -> None:
         super().__init__()
         self.backbone = getattr(torchvision.models, backbone)(pretrained=True)
         self.backbone = nn.Sequential(*list(self.backbone.children())[:-2])  # Remove fully connected layers
@@ -79,7 +116,10 @@ class DeTr(nn.Module):
         self.pos_embedding = nn.Embedding(num_embeddings=(self.seq_len), embedding_dim=C)
 
         self.encoder = nn.Sequential(
-            *(TransformerEncoder(hidden_dim=C, fc_dim=fc_dim, num_heads=num_heads, activation=activ_fn) for _ in range(num_encoder))
+            *(TransformerEncoder(C, fc_dim, num_heads, activ_fn) for _ in range(num_encoder))
+        )
+        self.decoder = nn.Sequential(
+            *(TransformerDecoder(C, fc_dim, num_heads, num_obj, activ_fn) for _ in range(num_decoder))
         )
 
 
@@ -97,6 +137,7 @@ class DeTr(nn.Module):
         pos_embedding = self.pos_embedding(torch.arange(self.seq_len, device=x.device))
         x += pos_embedding
         x = self.encoder(x)
+        x = self.decoder(x)
 
 
         return x
@@ -112,7 +153,9 @@ if __name__ == "__main__":
     activ_fn = "relu"
     num_heads = 8
     fc_dim = 256
-    model = DeTr(backbone, input_shape, fc_dim, num_heads, activ_fn, num_encoder)
+    num_decoder = 2
+    num_obj = 128
+    model = DeTr(backbone, input_shape, fc_dim, num_heads, activ_fn, num_encoder, num_decoder, num_obj)
     x = torch.randn(1, *input_shape)
     x = x.to(device)
     model = model.to(device)
